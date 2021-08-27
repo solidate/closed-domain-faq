d3207e29e24f6356ad8d28d4978e44e65a384d07
Keshav Kumar <keshavsharma26.7@gmail.com>
2021-08-27 23:35:29 +0530

    Flask

--- app.py
+++ app.py
@@ -1,18 +1,56 @@
-import requests
-import json
-import streamlit as st
+from flask import Flask, request, current_app
+from haystack import Finder
+from haystack.document_store.memory import InMemoryDocumentStore
+from haystack.retriever.dense import EmbeddingRetriever
+from haystack.utils import print_answers
+from haystack.pipeline import FAQPipeline
+import pandas as pd
 
-def get_answer(query:str):
-    response = requests.post('http://0.0.0.0:8000/search',json={'query':query})
-    response = response.json()
-    return response['answer']
+app = Flask(__name__)
 
-def main():
-    st.title("Closed domain FAQ System Demo")
-    st.text('Please enter your query')
-    user_input = st.text_area("Search box")
-    output = get_answer(user_input)
-    st.write(output)
+def _document_store(similarity="cosine",index="document",
+                                            embedding_field="question_emb",
+                                            embedding_dim=768):
+    document_store = InMemoryDocumentStore(similarity=similarity,index=index,
+                                            embedding_field=embedding_field,
+                                            embedding_dim=embedding_dim)
+    
+    return document_store
 
-if __name__=="__main__":
-    main()
+def _document_retriever(document_store=None,embedding_model=None):
+    retriever = EmbeddingRetriever(document_store=document_store, 
+                                    embedding_model=embedding_model)
+
+    return retriever
+
+def _faq_pipeline(retriever=None):
+    pipeline = FAQPipeline(retriever=retriever)
+
+    return pipeline
+
+@app.before_first_request
+def startup_event():
+    global store,retr,pipe
+    store = _document_store()
+    retr = _document_retriever(document_store=store, embedding_model="sentence-transformers/paraphrase-albert-small-v2")
+    pipe =  _faq_pipeline(retriever=retr)
+
+    df = pd.read_csv('FAQ/FAQ_data.csv')
+    questions = list(df["question"].values)
+    df["question_emb"] = retr.embed_queries(texts=questions)
+    df = df.rename(columns={"question": "text"})
+    
+    docs_to_index = df.to_dict(orient="records")
+    store.write_documents(docs_to_index)
+
+@app.route("/")
+def index():
+    return current_app.send_static_file("index.html")
+
+@app.route('/search/',methods=['POST'])
+def search():
+    query = request.get_json()['query']
+    global pipe
+    prediction = pipe.run(query=query, top_k_retriever=1)
+    prediction = prediction['answers'][0]['answer']
+    return {'answer':prediction}
